8a83d132edc45e29ee82d335373e8120dd70e8b8
Vladimir Ermakov <vooon341@gmail.com>
2009-06-22 00:50:08 +0400

    servo_gpio: format code

--- drivers/servo_gpio/servo_driver.c
+++ drivers/servo_gpio/servo_driver.c
@@ -55,14 +55,14 @@
 static uint16_t gpio_servo_pos[16];
 static bool gpio_servo_enb[16];
 
-//static GATE_RESULT driver_read(uint8_t reg, uint8_t* data, uint8_t* data_len);
+static GATE_RESULT driver_read(uint8_t reg, uint8_t* data, uint8_t* data_len);
 static GATE_RESULT driver_write(uint8_t reg, uint8_t* data, uint8_t data_len);
 
 static GATE_DRIVER driver = {
 	.uid = 0x0031,
 	.major_version = 1,
 	.minor_version = 0,
-//	.read = driver_read,
+	.read = driver_read,
 	.write = driver_write,
 	.num_registers = 2,
 };
@@ -74,135 +74,159 @@ uint8_t mask1[40];
 uint8_t iterator;
 
 uint8_t port_mask[16] = {
-	1<<0,
-	1<<1,
-	1<<2,
-	1<<3,
-	1<<4,
-	1<<5,
-	1<<6,
-	1<<7,
-	1<<7,
-	1<<6,
-	1<<5,
-	1<<4,
-	1<<3,
-	1<<2,
-	1<<5,
-	1<<4
+	_BV(0), _BV(1),
+	_BV(2), _BV(3),
+	_BV(4), _BV(5),
+	_BV(6), _BV(7),
+	_BV(7), _BV(6),
+	_BV(5), _BV(4),
+	_BV(3), _BV(2),
+	_BV(5), _BV(4)
 };
 
-#define portHandlerOR(name,port,nextHandler,id) \
-void name(void)\
-{\
-   port |= mask0[id]; \
-   OCR2 += pause0[id]; \
-   handler = nextHandler; \
-}
+#define portHandlerOR(name, port, nextHandler, id) \
+	void name(void) \
+	{ \
+		port |= mask0[id]; \
+		OCR2 += pause0[id]; \
+		handler = nextHandler; \
+	}
 
-#define portHandlerAND(name,port,nextHandler,id) \
-void name(void)\
-{\
-   port &= mask0[id]; \
-   OCR2 += pause0[id]; \
-   for(iterator=0; iterator<pause1[id]; iterator++){
-     delay_us(1);
-   };
-   port &= mask1[id]; \
-   handler = nextHandler; \
-}
+#define portHandlerAND(name, port, nextHandler, id) \
+	void name(void) \
+	{ \
+		port &= mask0[id]; \
+		OCR2 += pause0[id]; \
+		for (iterator=0; iterator<pause1[id]; iterator++) { \
+			delay_us(1); \
+		} \
+		port &= mask1[id]; \
+		handler = nextHandler; \
+	}
 
-#define portHandlers(process,port,nextProcess,id) \
-portHandlerAND(##process##4,port,##nextProcess##0,(id+4))\
-portHandlerAND(##process##3,port,##process##4,(id+3))\
-portHandlerAND(##process##2,port,##process##3,(id+2))\
-portHandlerAND(##process##1,port,##process##2,(id+1))\
-portHandlerOR(##process##0,port,##process##1,(id))\
+#define portHandlers(process, port, nextProcess, id) \
+	portHandlerAND(process##4, port, nextProcess##0, (id+4)) \
+	portHandlerAND(process##3, port, process##4,     (id+3)) \
+	portHandlerAND(process##2, port, process##3,     (id+2)) \
+	portHandlerAND(process##1, port, process##2,     (id+1)) \
+	portHandlerOR (process##0, port, process##1,     (id))
 
 void processD0(void);
 
-portHandlers(processAa, PORTA, processD, 0)
-portHandlers(processAb, PORTA, processAa, 4)
-portHandlers(processAc, PORTA, processAb, 8)
-portHandlers(processAd, PORTA, processAc, 12)
-portHandlers(processCa, PORTC, processAd, 16)
-portHandlers(processCb, PORTC, processCa, 20)
-portHandlers(processB, PORTB, processCb, 24)
-portHandlers(processD, PORTD, processB, 28)
+portHandlers(processAa, PORTA, processD,  0);
+portHandlers(processAb, PORTA, processAa, 4);
+portHandlers(processAc, PORTA, processAb, 8);
+portHandlers(processAd, PORTA, processAc, 12);
+portHandlers(processCa, PORTC, processAd, 16);
+portHandlers(processCb, PORTC, processCa, 20);
+portHandlers(processB,  PORTB, processCb, 24);
+portHandlers(processD,  PORTD, processB,  28);
 
-#define generateParams(port_id, param_id) \
-uint8_t maskX0=0,maskX1=0; \
-uint16_t pauseX0=0, pauseX1=0; \
-if(gpio_servo_enb[port_id]){ maskX0=port_mask[port_id]; pauseX0=gpio_servo_pos[port_id]; }; \
-if(gpio_servo_enb[port_id+1]){ maskX1=port_mask[port_id+1]; pauseX1=gpio_servo_pos[port_id+1]; }; \
-if(pauseX0>pauseX1){ \
-  uint8_t tmp8=pauseX0; pauseX0=pauseX1; pauseX1=tmp8; \
-  uint16_t tmp16=maskX0; maskX0=maskX1; maskX1=tmp16; \
-}; \
-mask0[param_id]=maskX0|maskX1; \
-if(pauseX1-pauseX0<2){ \
-  if(pauseX0>400){ \
-    pause0[0]=190; mask0[0]=maskX0|maskX1; \
-	pause0[1]=190; mask0[1]=0xFF; pause1[1]=0; mask1[1]=0xFF; \
-	pause0[2]=pauseX0-380; mask0[2]=0xFF; pause1[2]=0; mask1[2]=0xFF; \
-	if(pauseX0==pauseX1){ \
-	  pause0[3]=590-pauseX0; mask0[3]=~(maskX0|maskX1); pause1[3]=0; mask1[3]=0xFF; \
-    }else{ \
-      pause0[3]=590-pauseX0; mask0[3]=~maskX0; pause1[3]=pauseX1-pauseX0; mask1[3]=~maskX1; \
-    }; \
-	pause0[4]=10; mask0[4]=0xFF; pause1[4]=0; mask1[4]=0xFF; \
-  }else if(pauseX0>200){ \
-    pause0[0]=190; mask0[0]=maskX0|maskX1; \
-	pause0[1]=pauseX0-190; mask0[1]=0xFF; pause1[1]=0; mask1[1]=0xFF; \
-	if(pauseX0==pauseX1){ \
-	  pause0[2]=90; mask0[2]=~(maskX0|maskX1); pause1[2]=0; mask1[2]=0xFF; \
-    }else{ \
-	  pause0[2]=90; mask0[2]=~maskX0; pause1[2]=pauseX1-pauseX0; mask1[2]=~maskX1; \
-    };
-	pause0[3]=420-pauseX0; mask0[3]=0xFF; pause1[3]=0; mask1[3]=0xFF; \
-	pause0[4]=90; mask0[4]=0xFF; pause1[4]=0; mask1[4]=0xFF; \
-  }else{ \
-    pause0[0]=pauseX0; mask0[0]=maskX0|maskX1; \
-	if(pauseX0==pauseX1){ \
-      pause0[1]=250-pauseX0; mask0[1]=~(maskX0|maskX1); pause1[1]=0; mask1[1]=0xFF; \
-	}else{
-	  pause0[1]=250-pauseX0; mask0[1]=~maskX0; pause1[1]=pauseX1-pauseX0; mask1[1]=~maskX1; \
-    };
-	pause0[2]=100; mask0[2]=0xFF; pause1[2]=0; mask1[2]=0xFF; \
-	pause0[3]=150; mask0[3]=0xFF; pause1[3]=0; mask1[3]=0xFF; \
-	pause0[4]=100; mask0[4]=0xFF; pause1[4]=0; mask1[4]=0xFF; \
-  }; \
-}else{ \
-  if(pauseX1-pauseX0>235){ \
-	pause0[0]=110; mask0[0]=maskX0|maskX1; \
-	pause0[1]=pauseX0-110; mask0[1]=0xFF; pause1[1]=0; mask1[1]=0xFF; \
-	pause0[2]=230; mask0[2]=~maskX0; pause1[2]=0; mask1[2]=0xFF; \
-	pause0[3]=pauseX1-pauseX0-230; mask0[3]=0xFF; pause1[3]=0; mask1[3]=0xFF; \
-	pause0[4]=600-pauseX1; mask0[4]=~maskX1; pause1[4]=0; mask1[4]=0xFF; \
-  }else{ \
-    if(pauseX0>440){ \
-      pause0[0]=210; mask0[0]=maskX0|maskX1; \
-	  pause0[1]=210; mask0[1]=0xFF; pause1[1]=0; mask1[1]=0xFF; \
-	  pause0[2]=pauseX0-420; mask0[2]=0xFF; pause1[2]=0; mask1[2]=0xFF; \
-	  pause0[3]=pauseX1-pauseX0; mask0[3]=~maskX0; pause1[3]=0; mask1[3]=0xFF; \
-	  pause0[4]=600-pauseX1; mask0[4]=~maskX1; pause1[4]=0; mask1[4]=0xFF; \
-    }else if(pauseX0>220){ \
-      pause0[0]=210; mask0[0]=maskX0|maskX1; \
-	  pause0[1]=pauseX0-210; mask0[1]=0xFF; pause1[1]=0; mask1[1]=0xFF; \
-	  pause0[2]=pauseX1-pauseX0; mask0[2]=~maskX0; pause1[2]=0; mask1[2]=0xFF; \
-	  uint16_t paus=600-pauseX1;
-	  pause0[3]=paus >> 1; mask0[3]=~maskX1; pause1[3]=0; mask1[3]=0xFF; \
-	  pause0[4]=paus-(pause >> 1); mask0[4]=0xFF; pause1[4]=0; mask1[4]=0xFF; \
-    }else{ \
-      pause0[0]=10; mask0[0]=maskX0|maskX1; \
-	  pause0[1]=pauseX0-10; mask0[1]=0xFF; pause1[1]=0; mask1[1]=0xFF; \
-	  pause0[2]=pauseX1-pauseX0; mask0[2]=~maskX0; pause1[2]=0; mask1[2]=0xFF; \
-	  uint16_t paus=600-pauseX1;
-	  pause0[3]=paus >> 1; mask0[3]=~maskX1; pause1[3]=0; mask1[3]=0xFF; \
-	  pause0[4]=paus-(pause >> 1); mask0[4]=0xFF; pause1[4]=0; mask1[4]=0xFF; \
-    }; \
-  }; \
-};
+static inline generateParams(const unit8_t port_id, const uint8_t param_id)
+{
+	uint8_t maskX0=0,
+			maskX1=0;
+	uint16_t pauseX0=0,
+			 pauseX1=0;
+	
+	if (gpio_servo_enb[port_id]) {
+		maskX0 = port_mask[port_id]; 
+		pauseX0 = gpio_servo_pos[port_id];
+	}
+
+	if (gpio_servo_enb[port_id+1]) {
+		maskX1 = port_mask[port_id+1];
+		pauseX1 = gpio_servo_pos[port_id+1];
+	}
+
+	if (pauseX0 > pauseX1) {
+		uint16_t tmp16=pauseX0; 
+		pauseX0 = pauseX1; 
+		pauseX1 = tmp16;
+
+		uint8_t tmp8=maskX0;
+		maskX0 = maskX1;
+		maskX1 = tmp8;
+	}
+
+	mask0[param_id] = maskX0 | maskX1;
+
+	if (pauseX1-pauseX0 < 2) {
+		if (pauseX0 > 400) {
+			pause0[0] = 190;         mask0[0] = maskX0 | maskX1;
+			pause0[1] = 190;         mask0[1] = 0xFF; pause1[1] = 0; mask1[1] = 0xFF;
+			pause0[2] = pauseX0-380; mask0[2] = 0xFF; pause1[2] = 0; mask1[2] = 0xFF;
+
+			if (pauseX0 == pauseX1) {
+				pause0[3] = 590-pauseX0; mask0[3] = ~(maskX0|maskX1); pause1[3] = 0; mask1[3] = 0xFF;
+			} else {
+				pause0[3] = 590-pauseX0; mask0[3] = ~maskX0; pause1[3] = pauseX1-pauseX0; mask1[3] = ~maskX1;
+			}
+
+			pause0[4] = 10; mask0[4] = 0xFF; pause1[4] = 0; mask1[4] = 0xFF;
+			
+		} else if (pauseX0 > 200) {
+			pause0[0] = 190;         mask0[0] = maskX0 | maskX1;
+			pause0[1] = pauseX0-190; mask0[1] = 0xFF; pause1[1] = 0; mask1[1] = 0xFF;
+			
+			if (pauseX0 == pauseX1) {
+				pause0[2] = 90; mask0[2] = ~(maskX0|maskX1); pause1[2] = 0; mask1[2] = 0xFF;
+			} else {
+				pause0[2] = 90; mask0[2] = ~maskX0; pause1[2] = pauseX1-pauseX0; mask1[2] = ~maskX1;
+			}
+			
+			pause0[3] = 420-pauseX0; mask0[3] = 0xFF; pause1[3] = 0; mask1[3] = 0xFF;
+			pause0[4] = 90;          mask0[4] = 0xFF; pause1[4] = 0; mask1[4] = 0xFF;
+
+		} else {
+			pause0[0] = pauseX0; mask0[0] = maskX0 | maskX1;
+			
+			if (pauseX0 == pauseX1) {
+				pause0[1] = 250-pauseX0; mask0[1] = ~(maskX0|maskX1); pause1[1] = 0; mask1[1] = 0xFF;
+			} else {
+				pause0[1] = 250-pauseX0; mask0[1] = ~maskX0; pause1[1] = pauseX1-pauseX0; mask1[1] = ~maskX1;
+			}
+			
+			pause0[2] = 100; mask0[2] = 0xFF; pause1[2] = 0; mask1[2] = 0xFF;
+			pause0[3] = 150; mask0[3] = 0xFF; pause1[3] = 0; mask1[3] = 0xFF;
+			pause0[4] = 100; mask0[4] = 0xFF; pause1[4] = 0; mask1[4] = 0xFF;
+		}
+
+	} else {
+		if (pauseX1-pauseX0 > 235) {
+			pause0[0] = 110;                 mask0[0] = maskX0 | maskX1;
+			pause0[1] = pauseX0-110;         mask0[1] = 0xFF;    pause1[1] = 0; mask1[1] = 0xFF;
+			pause0[2] = 230;                 mask0[2] = ~maskX0; pause1[2] = 0; mask1[2] = 0xFF;
+			pause0[3] = pauseX1-pauseX0-230; mask0[3] = 0xFF;    pause1[3] = 0; mask1[3] = 0xFF;
+			pause0[4] = 600-pauseX1;         mask0[4] = ~maskX1; pause1[4] = 0; mask1[4] = 0xFF;
+
+		} else {
+			if (pauseX0 > 440) {
+				pause0[0] = 210;             mask0[0] = maskX0 | maskX1;
+				pause0[1] = 210;             mask0[1] = 0xFF;    pause1[1] = 0; mask1[1] = 0xFF;
+				pause0[2] = pauseX0-420;     mask0[2] = 0xFF;    pause1[2] = 0; mask1[2] = 0xFF;
+				pause0[3] = pauseX1-pauseX0; mask0[3] = ~maskX0; pause1[3] = 0; mask1[3] = 0xFF;
+				pause0[4] = 600-pauseX1;     mask0[4] = ~maskX1; pause1[4] = 0; mask1[4] = 0xFF;
+
+			} else if (pauseX0 > 220) {
+				uint16_t paus=600-pauseX1;
+				pause0[0] = 210;             mask0[0] = maskX0 | maskX1;
+				pause0[1] = pauseX0-210;     mask0[1] = 0xFF;    pause1[1] = 0; mask1[1] = 0xFF;
+				pause0[2] = pauseX1-pauseX0; mask0[2] = ~maskX0; pause1[2] = 0; mask1[2] = 0xFF;		
+				pause0[3] = paus >> 1;       mask0[3] = ~maskX1; pause1[3] = 0; mask1[3] = 0xFF;
+				pause0[4] = paus-(pause>>1); mask0[4] = 0xFF;    pause1[4] = 0; mask1[4] = 0xFF;
+
+			} else {
+				uint16_t paus=600-pauseX1;
+				pause0[0] = 10;              mask0[0] = maskX0 | maskX1;
+				pause0[1] = pauseX0-10;      mask0[1] = 0xFF;    pause1[1] = 0; mask1[1] = 0xFF;
+				pause0[2] = pauseX1-pauseX0; mask0[2] = ~maskX0; pause1[2] = 0; mask1[2] = 0xFF;
+				pause0[3] = paus >> 1;       mask0[3] = ~maskX1; pause1[3] = 0; mask1[3] = 0xFF;
+				pause0[4] = paus-(pause>>1); mask0[4] = 0xFF;    pause1[4] = 0; mask1[4] = 0xFF;
+			}
+		}
+	}
+}
 
 
 ISR(SIG_OUTPUT_COMPARE2)
@@ -210,16 +234,16 @@ ISR(SIG_OUTPUT_COMPARE2)
    handler();
 }
 
-void generateParameters(void)
+static inline void generateParameters(void)
 {
-  generateParams(0,0);
-  generateParams(2,5);
-  generateParams(4,10);
-  generateParams(6,15);
-  generateParams(8,20);
-  generateParams(10,25);
-  generateParams(12,30);
-  generateParams(14,35);
+  generateParams(0,  0);
+  generateParams(2,  5);
+  generateParams(4,  10);
+  generateParams(6,  15);
+  generateParams(8,  20);
+  generateParams(10, 25);
+  generateParams(12, 30);
+  generateParams(14, 35);
 };
 
 static inline void set_enable(uint8_t n, bool enable)
@@ -261,13 +285,11 @@ static inline void set_position(uint8_t n, uint32_t pos)
 }
 
 
-/* comment out now
 static GATE_RESULT driver_read(uint8_t reg, uint8_t* data, uint8_t* data_len)
 {
 	*data_len = 0;
 	return GR_OK;
 }
-*/
 
 static GATE_RESULT driver_write(uint8_t reg, uint8_t* data, uint8_t data_len)
 {
