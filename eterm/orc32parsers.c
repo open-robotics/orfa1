89f411409001aa70130e1ef0d7959c39757f35aa
Vladimir Ermakov <vooon341@gmail.com>
2010-09-29 00:48:36 +0400

    style fix; add to changelog
    --HG--
    branch : new_parser

--- eterm/orc32parsers.c
+++ eterm/orc32parsers.c
@@ -15,14 +15,13 @@
 #define debug(...)
 #endif
 
+typedef enum {
+	SMP_GET_COMMAND,			///< get command ( '#', 'P', 'S', 'T', '\r', '\n' )
+	SMP_ERROR,					///< skip all chars because command error, wait for '\r' or '\n'
+	SMP_PARSE_NUMBER,			///< parse number after command
+} state_cmd_t;
+
 static bool servo_move_parser(char c, bool reinit) {
-
-	typedef enum {
-		SMP_GET_COMMAND,			///< get command ( '#', 'P', 'S', 'T', '\r', '\n' )
-		SMP_ERROR,					///< skip all chars because command error, wait for '\r' or '\n'
-		SMP_PARSE_NUMBER,			///< parse number after command
-	} state_cmd_t;
-
 	static state_cmd_t state_cmd;
 	static uint16_t _servo_target[SERVO_LEN];
 	static uint16_t _servo_maxspeed[SERVO_LEN];
@@ -31,48 +30,47 @@ static bool servo_move_parser(char c, bool reinit) {
 	static uint16_t _num=0;
 	static uint8_t _cmd=' ';
 
-	//printf("smp c=%c init=%d state=%d\n",c,reinit,state_cmd);
+	//debug("%% smp c=%c init=%d state=%d\n", c, reinit, state_cmd);
 	
-	if(reinit)
-	{
+	if (reinit) {
 		// Clear machine
-		for (uint8_t i=0; i<SERVO_LEN; i++) {
+		for (uint8_t i=0; i < SERVO_LEN; i++) {
 			_servo_target[i] = 0;
 			_servo_maxspeed[i] = 0;
-		};
-		state_cmd=SMP_GET_COMMAND;
-		_time2go=0;
-		_cmd=' ';
-		_servo=0;
-		_num=0;
-	};
+		}
+		state_cmd = SMP_GET_COMMAND;
+		_time2go = 0;
+		_cmd = ' ';
+		_servo = 0;
+		_num = 0;
+	}
 
-	switch(state_cmd) {
+	switch (state_cmd) {
 		case SMP_PARSE_NUMBER:
 			if (c >= '0' && c <= '9') {
-				_num = _num*10 + (c-'0');
+				_num = _num*10 + (c - '0');
 				state_cmd = SMP_PARSE_NUMBER;
 			} else {
-				if (_cmd == '#')
+				if (_cmd == '#') {
 					if(_num < SERVO_LEN)
 						_servo = _num;
-				if (_cmd == 'P')
+				} else if (_cmd == 'P') {
 					if (_num <= 2500 && _num >= 500) {
-					_servo_target[_servo] = _num;
-					debug("# pos[%d]=%d\n", _servo, _num);
-				}
-				if (_cmd == 'S') {
+						_servo_target[_servo] = _num;
+						debug("%% pos[%d]=%d\n", _servo, _num);
+					}
+				} else if (_cmd == 'S') {
 					_servo_maxspeed[_servo] = _num;
-					debug("# spd[%d]=%d\n", _servo, _num);
-				}
-				if (_cmd == 'T') {
+					debug("%% spd[%d]=%d\n", _servo, _num);
+				} else if (_cmd == 'T') {
 					_time2go = _num;
-					debug("# time=%d\n", _num);
+					debug("%% time=%d\n", _num);
 				}
 				state_cmd = SMP_GET_COMMAND;
 			}
-			if (state_cmd != SMP_GET_COMMAND)
-				break;
+
+		if (state_cmd != SMP_GET_COMMAND)
+			break;
 
 		case SMP_GET_COMMAND:
 			switch(c) {
@@ -89,7 +87,8 @@ static bool servo_move_parser(char c, bool reinit) {
 					return false;
 
 				case '\n':
-					if (_cmd != ' ') servo_command(_time2go, _servo_target, _servo_maxspeed);
+					if (_cmd != ' ')
+						servo_command(_time2go, _servo_target, _servo_maxspeed);
 					return true;
 
 				default:
@@ -104,79 +103,77 @@ static bool servo_move_parser(char c, bool reinit) {
 		
 	}
 
-	if( c != '\n' ) return false;
+	if (c != '\n')
+		return false;
 	printf("ERR in # cmd\n");
 	return true;
 }
 
-static bool query_status_parser(char c, bool reinit) {
 
-#define QSP_SELECT_CMD 100
-#define QSP_Q_ERROR 101
+#define QSP_SELECT_CMD  100
+#define QSP_Q_ERROR     101
 #define QSP_QP_SN_ERROR 102
 
+static bool query_status_parser(char c, bool reinit) {
 	static uint8_t servo_num;
 
-	if(reinit){
-		servo_num=QSP_SELECT_CMD;
+	if (reinit) {
+		servo_num = QSP_SELECT_CMD;
 		return false;
-	};
+	}
 
-	if( servo_num==QSP_SELECT_CMD )
-	{
-		if( c=='\n' )
-		{
-			if( servo_lld_is_done() )
-			{
+	if (servo_num == QSP_SELECT_CMD) {
+		if (c == '\n') {
+			if (servo_is_done()) {
 				printf(".");
-			}else{
+			} else {
 				printf("+");
-			};
+			}
 			return true;
-		};
-		if( c=='P' )
-		{
-			servo_num=0;
+		}
+		if (c == 'P') {
+			servo_num = 0;
 			return false;
 		}
-		if( c!=' ' ) servo_num=QSP_Q_ERROR;
+		if (c != ' ')
+			servo_num = QSP_Q_ERROR;
 		return false;
-	};
+	}
 
-	if( servo_num==QSP_Q_ERROR )
-	{
-		if( c!='\n' ) return false;
+	if (servo_num == QSP_Q_ERROR) {
+		if (c != '\n')
+			return false;
 		printf("ERR in Q cmd\n");
 		return true;
-	};
-
-	if( servo_num==QSP_QP_SN_ERROR )
-	{
-		if( c!='\n' ) return false;
+	}
+	
+	if (servo_num == QSP_QP_SN_ERROR) {
+		if (c != '\n')
+			return false;
 		printf("ERR in QP cmd servo #\n");
 		return true;
-	};
+	}
 
-	if( servo_num<100 ){
-		if( c>='0' && c<='9' )
-		{
-			if(servo_num>=10)
-			{
-				servo_num=QSP_QP_SN_ERROR;
+	if (servo_num < 100) {
+		if (c >= '0' && c <= '9') {
+			if (servo_num >= 10) {
+				servo_num = QSP_QP_SN_ERROR;
 				return false;
-			};
-			servo_num=servo_num*10+(c-'0');
+			}
+			servo_num = servo_num * 10 + (c - '0');
 			return false;
-		};
-		if( c=='\n' )
-		{
-			char r=servo_get_position(servo_num)/10;
+		}
+
+		if (c == '\n') {
+			char r = servo_get_position(servo_num)/10;
 			putchar(r);
 			return true;
-		};
-		if( c!=' ' ) servo_num=QSP_QP_SN_ERROR;
+		}
+
+		if (c != ' ')
+			servo_num = QSP_QP_SN_ERROR;
 		return false;
-	};
+	}
 
 	return c == '\n';
 }
@@ -187,6 +184,8 @@ static parser_t orc32parsers[] = {
 };
 
 void register_orc32(void) {
-	register_parser(&orc32parsers[0]);
+	for (uint8_t i=0; i < ARRAY_SIZE(orc32parsers); i++) {
+		register_parser(orc32parsers + i);
+	}
 }
 
