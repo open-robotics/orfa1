a3821cc1d0357347b1405b8ca4205796fdee60f6
Anton Botov <airsoft_ekb@mail.ru>
2010-09-29 02:07:45 +0600

    Add # cmd parser for SSC-32 protocol (untested)
    --HG--
    branch : new_parser

--- eterm/orc32parsers.c
+++ eterm/orc32parsers.c
@@ -7,13 +7,105 @@
 #include "eterm.h"
 #include "hal/servo.h"
 
-static bool servo_move_parser(char c, bool reinit) {
-//	uint8_t servo;
-//	uint16_t position;
-//	uint16_t speed;
-//	uint16_t time;
+/// Debug print
+#ifndef NDEBUG
+#include <stdio.h>
+#define debug(...) printf(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
 
-	return c == '\n';
+static bool servo_move_parser(char c, bool reinit) {
+
+	typedef enum {
+		SMP_GET_COMMAND,			///< get command ( '#', 'P', 'S', 'T', '\r', '\n' )
+		SMP_ERROR,					///< skip all chars because command error, wait for '\r' or '\n'
+		SMP_PARSE_NUMBER,			///< parse number after command
+	} state_cmd_t;
+
+	static state_cmd_t state_cmd;
+	static uint16_t _servo_target[SERVO_LEN];
+	static uint16_t _servo_maxspeed[SERVO_LEN];
+	static uint16_t _time2go;
+	static uint16_t _servo=0;
+	static uint16_t _num=0;
+	static uint8_t _cmd=' ';
+	
+	if(reinit)
+	{
+		// Clear machine
+		for (uint8_t i=0; i<SERVO_LEN; i++) {
+			_servo_target[i] = 0;
+			_servo_maxspeed[i] = 0;
+		};
+		state_cmd=SMP_GET_COMMAND;
+		_time2go=0;
+		_cmd=' ';
+		_servo=0;
+		_num=0;
+		return false;
+	};
+
+	switch(state_cmd) {
+		case SMP_PARSE_NUMBER:
+			if (c >= '0' && c <= '9') {
+				_num = _num*10 + (c-'0');
+				state_cmd = SMP_PARSE_NUMBER;
+			} else {
+				if (_cmd == '#')
+					if(_num < SERVO_LEN)
+						_servo = _num;
+				if (_cmd == 'P')
+					if (_num <= 2500 && _num >= 500) {
+					_servo_target[_servo] = _num;
+					debug("# pos[%d]=%d\n", _servo, _num);
+				}
+				if (_cmd == 'S') {
+					_servo_maxspeed[_servo] = _num;
+					debug("# spd[%d]=%d\n", _servo, _num);
+				}
+				if (_cmd == 'T') {
+					_time2go = _num;
+					debug("# time=%d\n", _num);
+				}
+				state_cmd = SMP_GET_COMMAND;
+			}
+			if (state_cmd != SMP_GET_COMMAND)
+				break;
+
+		case SMP_GET_COMMAND:
+			switch(c) {
+				case '#':
+				case 'P':
+				case 'S':
+				case 'T':
+					_cmd = c;
+					_num = 0;
+					state_cmd = SMP_PARSE_NUMBER;
+					return false;
+
+				case ' ':
+					return false;
+
+				case '\n':
+					if (_cmd != ' ') servo_command(_time2go, _servo_target, _servo_maxspeed);
+					return true;
+
+				default:
+					state_cmd = SMP_ERROR;
+					return false;
+			}
+			break;
+
+		default:
+			state_cmd = SMP_ERROR;
+			break;
+		
+	}
+
+	if( c != '\n' ) return false;
+	printf("ERR in # cmd\n");
+	return true;
 }
 
 static bool query_status_parser(char c, bool reinit) {
