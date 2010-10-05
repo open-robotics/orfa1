4ac7dffbb5576892fd6a10a79b64c88f38abdaea
Anton Botov <airsoft_ekb@mail.ru>
2010-10-05 23:50:49 +0600

    Pin control parser almost fixed
    --HG--
    branch : new_parser

--- eterm/orc32parsers.c
+++ eterm/orc32parsers.c
@@ -77,7 +77,10 @@ void pcp_mode(uint8_t _port, uint8_t _pin, uint8_t _mode)
 			adc_mask &= (1<<_pin) ^ 0xFF;
 		};
 		adc_reconfigure(adc_mask);
-		if(_mode=='A') return;
+		if(_mode=='A'){
+			printf("PinMode%c%d=ADC\n",_port,_pin);
+			return;
+		};
 	};
 
 	if(_mode=='A'){
@@ -85,8 +88,10 @@ void pcp_mode(uint8_t _port, uint8_t _pin, uint8_t _mode)
 		return;
 	}else if(_mode=='I'){
 		gate_port_config(_port_num, 1<<_pin, 0);
+		printf("PinMode%c%d=In\n",_port,_pin);
 	}else if(_mode=='O'){
 		gate_port_config(_port_num, 1<<_pin, 0xFF);		
+		printf("PinMode%c%d=Out\n",_port,_pin);
 	};
 }
 
@@ -105,6 +110,7 @@ void pcp_set(uint8_t _port, uint8_t _pin, uint8_t _value)
 	_value-='0';
 	if(_value) _value=0xFF;
 	gate_port_write(_port_num, 1<<_pin, _value);
+	printf("%c%d=%d\n",_port,_pin,_value & 0x01);
 }
 
 void pcp_get(uint8_t _port, uint8_t _pin)
@@ -128,54 +134,50 @@ void pcp_get(uint8_t _port, uint8_t _pin)
 			};
 			uint8_t volts=value/100;
 			uint8_t centivolts=value%100;
-			printf("%c%c:%d.%02d\n",_port,_pin,volts,centivolts);
+			printf("%c%d:%d.%02d\n",_port,_pin,volts,centivolts);
 			return;
 		};
 	};
 	uint8_t result=0;
 	gate_port_read(_port_num, &result);
 	if(result & (1<<_pin)){
-		_pin+='0';
-		printf("%c%c:1\n",_port,_pin);
+		printf("%c%d:1\n",_port,_pin);
 	}else{
-		_pin+='0';
-		printf("%c%c:0\n",_port,_pin);
+		printf("%c%d:0\n",_port,_pin);
 	};
 }
 
 typedef enum {
-	PCP_GET_COMMAND,			///< get command
-	PCP_GET_n,					///< get "n" in "Pin"
-	PCP_GET_t,					///< get "t" in "PinGet"
-	PCP_GET_d,					///< get "d" in "PinMode"
-	PCP_GET_e,					///< get "e" in "PinMode"
-	PCP_GET_PORT,				///< get port id
-	PCP_GET_PIN,				///< get pin id
-	PCP_GET_VALUE,				///< get value
-	PCP_WAIT_EOL,				///< skip all chars because command end, wait for '\r' or '\n'
-	PCP_ERROR,					///< skip all chars because command error, wait for '\r' or '\n'
+	PCP_GET_COMMAND,			//0/< get command
+	PCP_GET_n,					//1/< get "n" in "Pin"
+	PCP_GET_t,					//2/< get "t" in "PinGet"
+	PCP_GET_d,					//3/< get "d" in "PinMode"
+	PCP_GET_e,					//4/< get "e" in "PinMode"
+	PCP_GET_PORT,				//5/< get port id
+	PCP_GET_PIN,				//6/< get pin id
+	PCP_GET_VALUE,				//7/< get value
+	PCP_WAIT_EOL,				//8/< skip all chars because command end, wait for '\r' or '\n'
+	PCP_ERROR,					//9/< skip all chars because command error, wait for '\r' or '\n'
 } state_cmd_pcp;
 
 static bool pin_control_parser(char c, bool reinit) {
 	static state_cmd_pcp state_cmd;
 	static uint8_t _cmd=' ';
 	static uint8_t _port=' ';
-	static uint8_t _mode=' ';
 	static uint8_t _value=' ';
-	static uint8_t _pin=0;
+	static uint8_t _pin=' ';
 	
 	if (reinit) {
 		// Clear machine
 		state_cmd = PCP_GET_COMMAND;
 		_port = ' ';
-		_pin=0;
+		_pin=' ';
 		_value = ' ';
-		_mode = ' ';
 		_cmd=' ';
 		return false;
 	}
 
-	//printf("%% pcp c=%c init=%d state=%d\n", c, reinit, state_cmd);
+	//printf("*pcp c%c init%d st%d cm%c pt%c pn%c vl%c\n", c, reinit, state_cmd, _cmd, _port, _pin, _value);
 
 	switch (state_cmd) {
 		case PCP_GET_COMMAND:
@@ -190,7 +192,7 @@ static bool pin_control_parser(char c, bool reinit) {
 					state_cmd = PCP_GET_n;
 					return false;
 				case '\n':
-					printf("ERR in P cmd - wrong command\n");
+					printf("ERR01 in P cmd - wrong command\n");
 					return true;
 				default:
 					state_cmd = PCP_ERROR;
@@ -216,7 +218,7 @@ static bool pin_control_parser(char c, bool reinit) {
 					state_cmd = PCP_GET_d;
 					return false;
 				case '\n':
-					printf("ERR in P cmd - wrong port\n");
+					printf("ERR02 in P cmd - wrong port\n");
 					return true;
 				default:
 					state_cmd = PCP_ERROR;
@@ -234,12 +236,12 @@ static bool pin_control_parser(char c, bool reinit) {
 				case '5':
 				case '6':
 				case '7':
-					_pin = c-'0';
+					_pin = c;
 					state_cmd = PCP_GET_VALUE;
 					if(_cmd == 'G') state_cmd = PCP_WAIT_EOL;
 					return false;
 				case '\n':
-					printf("ERR in P cmd - wrong pin\n");
+					printf("ERR03 in P cmd - wrong pin\n");
 					return true;
 				default:
 					state_cmd = PCP_ERROR;
@@ -273,7 +275,7 @@ static bool pin_control_parser(char c, bool reinit) {
 					state_cmd = PCP_WAIT_EOL;
 					return false;
 				case '\n':
-					printf("ERR in P cmd - wrong value\n");
+					printf("ERR04 in P cmd - wrong value\n");
 					return true;
 				default:
 					state_cmd = PCP_ERROR;
@@ -287,15 +289,15 @@ static bool pin_control_parser(char c, bool reinit) {
 					return false;
 				case '\n':
 					if(_cmd=='M'){
-						pcp_mode(_port,_pin,_mode);
+						pcp_mode(_port,_pin-'0',_value);
 						return true;
 					};
 					if(_cmd=='S'){
-						pcp_set(_port,_pin,_value);
+						pcp_set(_port,_pin-'0',_value);
 						return true;
 					};
 					if(_cmd=='G'){
-						pcp_get(_port,_pin);
+						pcp_get(_port,_pin-'0');
 						return true;
 					};
 					return true;
@@ -309,11 +311,10 @@ static bool pin_control_parser(char c, bool reinit) {
 		case PCP_GET_n:
 			switch(c) {
 				case 'n':
-					_cmd = c;
 					state_cmd = PCP_GET_COMMAND;
 					return false;
 				case '\n':
-					printf("ERR in Pin cmd\n");
+					printf("ERR05 in Pin cmd\n");
 					return true;
 				default:
 					state_cmd = PCP_ERROR;
@@ -324,11 +325,10 @@ static bool pin_control_parser(char c, bool reinit) {
 		case PCP_GET_t:
 			switch(c) {
 				case 't':
-					_cmd = c;
 					state_cmd = PCP_GET_PORT;
 					return false;
 				case '\n':
-					printf("ERR in PinGet cmd\n");
+					printf("ERR06 in PinGet cmd\n");
 					return true;
 				default:
 					state_cmd = PCP_ERROR;
@@ -339,11 +339,10 @@ static bool pin_control_parser(char c, bool reinit) {
 		case PCP_GET_d:
 			switch(c) {
 				case 'd':
-					_cmd = c;
 					state_cmd = PCP_GET_e;
 					return false;
 				case '\n':
-					printf("ERR in PinMode cmd\n");
+					printf("ERR07 in PinMode cmd\n");
 					return true;
 				default:
 					state_cmd = PCP_ERROR;
@@ -354,11 +353,10 @@ static bool pin_control_parser(char c, bool reinit) {
 		case PCP_GET_e:
 			switch(c) {
 				case 'e':
-					_cmd = c;
 					state_cmd = PCP_GET_PORT;
 					return false;
 				case '\n':
-					printf("ERR in PinMode cmd\n");
+					printf("ERR08 in PinMode cmd\n");
 					return true;
 				default:
 					state_cmd = PCP_ERROR;
@@ -374,7 +372,7 @@ static bool pin_control_parser(char c, bool reinit) {
 
 	if (c != '\n')
 		return false;
-	printf("ERR in P cmd\n");
+	printf("ERR09 in P cmd\n");
 	return true;
 }
 
